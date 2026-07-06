a1cb2f554d553db1ef8df0f2161546ccca9a7636
3APA3A-3AHO3A <allvadim@yandex.ru>
2026-07-06 21:07:59 +0500

    Initial commit: production ready release

---
+++ logger.py
@@ -0,0 +1,21 @@
+import logging
+import sys
+
+def setup_logger():
+    logger = logging.getLogger("RoutingUpdater")
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        '%(asctime)s | %(levelname)-8s | %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+
+    if not logger.handlers:
+        logger.addHandler(console_handler)
+
+    return logger
+
+logger = setup_logger()
